67515427d19cdf008a78a35e82a8214eec5793ee
joris-mns <jorismns@mns.fr>
2025-09-04 09:43:37 +0200

    nom menu

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -29,14 +29,14 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
+          title: 'Tableau de bord',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
         }}
       />
       <Tabs.Screen
         name="explore"
         options={{
-          title: 'Explore',
+          title: 'Contact',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
